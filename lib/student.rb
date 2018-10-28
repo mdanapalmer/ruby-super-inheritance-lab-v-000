43991d7dda1fc2690ffe1c4a2556f61951ae7536
Dana Palmer <roiai4@hotmail.com>
2018-10-28 19:59:24 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 class Student
-  
-  def raise_hand
-   puts "Pick me!"
-  end
-  
+
   def hello
-   puts "Hey there! I'm so excited to learn stuff."
+    puts "Hey there! I'm so excited to learn stuff."
   end
-  
+
+  def raise_hand
+    puts "Pick me!"
+  end
+
 end
